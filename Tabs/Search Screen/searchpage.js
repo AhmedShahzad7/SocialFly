9e55bdbeefeee5dd0d782f65b4b7ee4d197b4034
Ahmed Shahzad <ahmedshahzad3082004@gmail.com>
2025-04-02 18:59:22 +0500

    Nav Bar

--- Tabs/Search Screen/searchpage.js
+++ Tabs/Search Screen/searchpage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function H() {
+export default function S() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Page!</Text>
+      <Text style={styles.text}>Search Page!</Text>
     </View>
   );
 }
--- Tabs/Settings Screen/Settings.js
+++ Tabs/Settings Screen/Settings.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function H() {
+export default function Sett() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Page!</Text>
+      <Text style={styles.text}>Settings Page!</Text>
     </View>
   );
 }
